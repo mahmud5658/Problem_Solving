0f5666a49e3f14ad464910e23c84c8fe2356a7b4
mahmud5658 <almahmuda5@gmail.com>
2024-02-04 21:44:56 +0600

    problem solving

--- Dart/code_mama/4.dart
+++ Dart/code_mama/4.dart
@@ -3,16 +3,12 @@ import 'dart:io';
 void main() {
   String input = stdin.readLineSync()!;
   List<String> inputList = input.split(" ");
-
   int a = int.parse(inputList[0]);
   int b = int.parse(inputList[1]);
-  int c = int.parse(inputList[2]);
 
-  if (a < b && a < c) {
-    print(a);
-  } else if (b < a && b < c) {
-    print(b);
-  } else {
-    print(c);
-  }
+  num? area = .5 * a * b;
+
+  int final_result = area.toInt();
+
+  print(final_result);
 }
