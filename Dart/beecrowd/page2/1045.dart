a0b9b09816d8d8b7ded8e2a7cf709826005af26c
mahmud5658 <almahmuda5@gmail.com>
2024-02-12 23:06:13 +0600

    problem solving

--- Dart/beecrowd/page2/1045.dart
+++ Dart/beecrowd/page2/1045.dart
@@ -1,7 +1,30 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   String input = stdin.readLineSync()!;
   List<String> inputList = input.split(" ");
   List<double> intList = inputList.map(double.parse).toList();
+  intList.sort(
+    (b, a) => a.compareTo(b),
+  );
+  double a = intList[0];
+  double b = intList[1];
+  double c = intList[2];
+  if (a >= (b + c)) {
+    print('NAO FORMA TRIANGULO');
+  } else if (pow(a, 2) == (pow(b, 2) + pow(c, 2))) {
+    print('TRIANGULO RETANGULO');
+  } else if (pow(a, 2) > (pow(b, 2) + pow(c, 2))) {
+    print('TRIANGULO OBTUSANGULO');
+  } else if (pow(a, 2) < (pow(b, 2) + pow(c, 2))) {
+    print('TRIANGULO ACUTANGULO');
+  }
+  if ((a == b) && (b == c) && (c == a)) {
+    print('TRIANGULO EQUILATERO');
+  } else if (((a == b) && (c != a)) ||
+      ((b == c) && (a != c)) ||
+      ((c == a) && (b != c))) {
+    print('TRIANGULO ISOSCELES');
+  }
 }
