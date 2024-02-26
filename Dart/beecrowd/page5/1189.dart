4236a12728d407aa9d66f602817e00c231921d12
mahmud5658 <almahmuda5@gmail.com>
2024-02-27 01:13:04 +0600

    Problem Solving

---
+++ Dart/beecrowd/page5/1189.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main() {
+  List<List<double>> matrix = List.generate(12, (_) => List.filled(12, 0));
+  String? t = stdin.readLineSync()!;
+  double sum = 0;
+  int count = 0;
+
+  for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[i].length; j++) {
+      matrix[i][j] = double.parse(stdin.readLineSync()!);
+    }
+  }
+
+  for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[i].length; j++) {
+      if (j < i && j < (matrix.length - 1) - i) {
+        sum += matrix[i][j];
+        count++;
+      }
+    }
+  }
+  if (t == 'S') {
+    print(sum.toStringAsFixed(1));
+  } else {
+    double avg = sum / count;
+    print(avg.toStringAsFixed(1));
+  }
+}
