6ed7af12b344da261546135f7200b5bec5492e28
mahmud5658 <almahmuda5@gmail.com>
2025-04-17 23:07:41 +0600

    leet code 33

---
+++ leet_code/33/33.py
@@ -0,0 +1,21 @@
+def search(nums, target):
+    left, right = 0, len(nums)-1
+    while left<=right:
+        mid = (left+right)//2
+        if nums[mid]==target:
+            return mid
+        if(nums[left]<=nums[mid]):
+            if nums[left]<=target< nums[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        else:
+            if nums[mid]< target <= nums[right]:
+                left = mid+1
+            else:
+                right = mid-1
+    return -1
+
+print(search([4,5,6,7,0,1,2], 0))
+print(search([4,5,6,7,0,1,2], 3))  
+print(search([1], 0))  
