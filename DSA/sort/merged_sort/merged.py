272d96654399a284d22a6b4f3a4ba682ec0dd888
mahmud5658 <almahmuda5@gmail.com>
2025-04-20 02:17:00 +0600

    leet code 88

---
+++ DSA/sort/merged_sort/merged.py
@@ -0,0 +1,28 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    merge(left_half, right_half)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+
+arr = [10, 20, 40, 100, 50]
+print("Original List: ", arr)
+sorted_arr = merge_sort(arr)
+print("Sorted List: ", sorted_arr)
